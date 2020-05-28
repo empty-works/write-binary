9107f7bf89270be14ae3c68135f5c208b51c866b
empty-works <pama.mark@gmail.com>
2020-05-28 07:24:19 +0200

    Opening ofstream file in binary mode.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main() {
+
+	std::string file_name {"test.bin"};
+	struct Person {
+		std::string name;
+		int age;
+	};
+
+	// Write to binary file.
+	std::ofstream output_file;
+	output_file.open(file_name, std::ios::binary);
 	return 0;
 }
