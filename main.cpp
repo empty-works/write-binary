80b3ef0b4215a6de12d5c032ca17f2e14a78f781
empty-works <pama.mark@gmail.com>
2020-05-28 08:00:13 +0200

    Read from the binary file. Still need to fix segmentation fault.

--- main.cpp
+++ main.cpp
@@ -15,11 +15,25 @@ int main() {
 	output_file.open(file_name, std::ios::binary);
 	if(!output_file) {
 		std::cout << "Error opening file." << std::endl;
+		return 1;
 	}
 
 	Person someone {"Henry", 32};
 	output_file.write(reinterpret_cast<char *>(&someone), sizeof(Person));
-	
-	
+	output_file.close();	
+
+	// Read from binary file.
+	std::ifstream input_file;
+	input_file.open(file_name, std::ios::binary);
+	if(!input_file) {
+		std::cout << "Error opening file." << std::endl;
+		return 1;
+	}
+
+	Person someone_cool {};
+	input_file.read(reinterpret_cast<char *>(&someone_cool), sizeof(Person));
+	std::cout << "Someone cool " << someone_cool.name << " " << someone_cool.age << std::endl;
+	input_file.close();	
+		
 	return 0;
 }
